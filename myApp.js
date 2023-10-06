95023febce5ed93951e8a24aa9a8ac64048c84a1
Ahmed Hodhod <123288862+AhmedHodhod18@users.noreply.github.com>
2023-10-06 18:39:13 +0300

    Add files via upload

---
+++ myApp.js
@@ -0,0 +1,65 @@
+let express = require('express');
+let app = express();
+require('dotenv').config();
+let bodyParser = require('body-parser');
+
+// Use body-parser to Parse POST Requests
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+// Implement a Root-Level Request Logger Middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+})
+
+// Chain Middleware to Create a Time Server
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.send({time: req.time});
+})
+
+// Get Route Parameter Input from the Client
+app.get('/:word/echo', (req, res) => {
+  const word = req.params.word;
+  res.json({echo: word});
+})
+
+
+
+// Get Query Parameter Input from the Client
+app.route('/name').get((req, res) => {
+  const firstName = req.query.first;
+  const lastName = req.query.last;
+  res.json({name: `${firstName} ${lastName}`});
+}).post((req, res) => {
+  var str = req.body.first + " " + req.body.last;
+  res.json({name: str});
+})
+
+// Get Data from POST Requests
+// app.post('/name',(req, res) => {
+//   var str = req.body.first + " " + req.body.last;
+//   res.json({name: str});
+// })
+
+// Serve an HTML File
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// Serve Static Assets
+app.use('/public', express.static(__dirname + '/public'));
+
+// Serve JSON on a Specific Route
+app.get('/json', (req, res) => {
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    res.json({"message": "HELLO JSON"});
+  } else {
+    res.json({"message": "Hello json"});
+  }
+});
+
+module.exports = app;
